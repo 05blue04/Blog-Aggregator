14aa9a1d65e6b59d2cba2663ea633ba9af469379
05blue04 <alexmax.pienkowski@gmail.com>
2025-07-29 23:31:04 -0500

    updated schema to include feeds table, added addFeed command that inserts into table

--- handler_agg.go
+++ handler_agg.go
@@ -2,6 +2,11 @@ package main
 
 import (
 	"context"
+	"fmt"
+	"time"
+
+	"github.com/05blue04/Blog-Aggregator/internal/database"
+	"github.com/google/uuid"
 )
 
 func handlerAgg(s *state, cmd command) error {
@@ -13,6 +18,36 @@ func handlerAgg(s *state, cmd command) error {
 	}
 
 	printRSS(rss)
+
+	return nil
+}
+
+func handlerAddFeed(s *state, cmd command) error {
+	if len(cmd.args) != 2 {
+		return fmt.Errorf("usage: %s <name> <url>", cmd.name)
+	}
+
+	u, err := s.db.GetUser(context.Background(), s.cfg.Username)
+	if err != nil {
+		return err
+	}
+
+	params := database.CreateFeedParams{
+		ID:        uuid.New(),
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+		Name:      cmd.args[0],
+		Url:       cmd.args[1],
+		UserID:    u.ID,
+	}
+
+	feed, err := s.db.CreateFeed(context.Background(), params)
+
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("Feed :%+v\n", feed)
 
 	return nil
 }
